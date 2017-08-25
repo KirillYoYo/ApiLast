e36d4f49082ef07912153511b1bed6b38cfbd02c
some-PC-user <kirillmyasnikovmail@gmail.com>
2017-08-25 17:19:24 +0400

    form complete

--- src/views/SearchForm/index.jsx
+++ src/views/SearchForm/index.jsx
@@ -1,16 +1,59 @@
 import React from 'react';
-
+import {Input, Button, Row, Col } from 'antd';
 
 export default class SearchForm extends React.Component {
 	constructor() {
 		super()
 	}
 
+	state = {
+		visible: false,
+		tableData: [],
+		columns: [],
+		searchText: ''
+	};
+
+	search() {
+		console.log(this.state.searchText)
+	}
+	onSearch(e) {
+		if (e.target.value === '') {
+			this.setState({
+				searchText: e.target.value
+			})
+		} else {
+			this.setState({
+				searchText: e.target.value
+			})
+		}
+
+	}
+
 	render() {
 		console.log('render')
 
 		return (
-			<div>Search form</div>
+			<div className="search-form-page">
+				<h1>Search form</h1>
+				<div className="search">
+					<Row>
+						<Col span={8}>
+							<Input
+								type="text"
+								placeholder="Искать..."
+								onChange={this.onSearch.bind(this)}
+							/>
+						</Col>
+						<Col span={16}>
+							<Button type="primary" onClick={this.search.bind(this)}>
+								Искать
+							</Button>
+						</Col>
+					</Row>
+
+
+				</div>
+			</div>
 		)
 	}
 }
